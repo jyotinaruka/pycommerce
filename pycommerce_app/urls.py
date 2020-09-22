b610e85431d3bd45ba87425736de28245cd030ec
Jyoti Singh <jn.naruka@gmail.com>
2020-09-22 13:26:43 -0700

    Added Product details page

--- pycommerce_app/urls.py
+++ pycommerce_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    #path('category/<int:id>', views.category),
+    path('product/<int:id>', views.product_detail),
     path('register_login', views.register_login)
-
 ]
--- pycommerce_app/views.py
+++ pycommerce_app/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render, HttpResponse, redirect
 def index(request):
     return render(request, 'index.html')
 
+
+def product_detail(request, id):
+    return render(request, 'product.html')
+
+
 def register_login(request):
     return render(request, 'register_login.html')
