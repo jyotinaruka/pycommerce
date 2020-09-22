dd21a44f4f1d2f5364fed4e8a5ebf836b24efca7
Jyoti Singh <jn.naruka@gmail.com>
2020-09-21 19:51:25 -0700

    Add basic layout
    Add basic layout with bootstrap and fontawesome

--- pycommerce_app/views.py
+++ pycommerce_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, render
 
 
 def index(request):
-    return HttpResponse('It works!')
+    return render(request, 'index.html')
