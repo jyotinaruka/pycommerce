cfb68b8f56058d231a0445c93a7d5d72f271a3d1
Jyoti Singh <jn.naruka@gmail.com>
2020-09-23 13:55:20 -0700

    added url for functionality

--- pycommerce_app/views.py
+++ pycommerce_app/views.py
@@ -3,61 +3,84 @@ from pycommerce_app.models import User, Product, Category, Order
 
 
 def index(request):
-    return render(request, 'index.html')
+    category = Category.objects.first()
+    return redirect(f"/category/{category.id}")
+
+
+def category(request, id):
+    categories = Category.objects.all()
+    products = Product.objects.filter(Categories__id=id)
+    context = {
+        "categories": categories,
+        "products": products
+    }
+    return render(request, 'index.html', context)
 
 
 def product_detail(request, id):
-    return render(request, 'product.html')
+    product = Product.objects.get(id=id)
+    context = {
+        'product': product
+    }
+    return render(request, 'product.html', context)
 
 
+# admin pages
 def register_login(request):
     return render(request, 'register_login.html')
+
 
 def all_products(request):
     all_products = Product.objects.all()
     context = {
-        "products" : all_products
+        "products": all_products
     }
 
     return render(request, 'dashboard/products.html', context)
 
+
 def showproduct(request, product_id):
-    this_product = Product.objects.get(id = product_id)
+    this_product = Product.objects.get(id=product_id)
     categories = Category.objects.all()
     context = {
-        "product" : this_product,
-        "categories" : categories
+        "product": this_product,
+        "categories": categories
     }
 
     return render(request, 'dashboard/singleproduct.html', context)
 
+
 def editproduct(request):
-    this_product = Product.objects.get(id = request.POST['product_id'])
+    this_product = Product.objects.get(id=request.POST['product_id'])
     this_product.title = request.POST['title']
     this_product.desc = request.POST['desc']
     # this_product.categories.name = request.POST['category']
     # new_category = request.POST['new_category']
+    this_product.image_url = request.POST['image_url']
     this_product.save()
     return redirect('dashboard/products')
+
 
 def newproduct(request):
     return render(request, 'dashboard/newproduct.html')
 
+
 def addnewproduct(request):
-    
+
     Product.objects.create(
-        title = request.POST['title'],
-        desc = request.POST['desc'],
-        price = request.POST['price'],
-        inventory = request.POST['inventory'],
-        quantity_sold = request.POST['quantity_sold'],
-        image_url = "placeholder"
+        title=request.POST['title'],
+        desc=request.POST['desc'],
+        price=request.POST['price'],
+        inventory=request.POST['inventory'],
+        quantity_sold=request.POST['quantity_sold'],
+        image_url="placeholder"
     )
 
     return redirect('dashboard/products')
 
+
 def deleteproduct(request):
-    this_product = Product.objects.get(id = request.POST['product_id'])
+    this_product = Product.objects.get(id=request.POST['product_id'])
     this_product.delete()
 
     return redirect('dashboard/products')
