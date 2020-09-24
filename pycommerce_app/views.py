6db01dcacabb7f423aec32798853a36bed7cd745
Jyoti Singh <jn.naruka@gmail.com>
2020-09-23 17:20:29 -0700

    Add all products link

--- pycommerce_app/views.py
+++ pycommerce_app/views.py
@@ -5,9 +5,15 @@ import bcrypt
 from django.contrib.auth.decorators import login_required
 from .decorators import allowed_users
 
+
 def index(request):
-    category = Category.objects.first()
-    return redirect(f"/category/{category.id}")
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context = {
+        "categories": categories,
+        "products": products
+    }
+    return render(request, 'index.html', context)
 
 
 def category(request, id):
@@ -18,6 +24,7 @@ def category(request, id):
         "products": products
     }
     return render(request, 'index.html', context)
+
 
 def product_detail(request, id):
     product = Product.objects.get(id=id)
@@ -42,8 +49,10 @@ def thank_you_page(request):
 
 # admin pages
 
+
 def register_login(request):
     return render(request, 'register_login.html')
+
 
 def register(request):
     errors = Customer.objects.register_validator(request.POST)
@@ -55,15 +64,16 @@ def register(request):
     password = request.POST['password']
     pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
     new_customer = Customer.objects.create(
-        first_name = request.POST['first_name'],
-        last_name = request.POST['last_name'],
-        email = request.POST['email'],
-        password = pw_hash,
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        password=pw_hash,
     )
-    
+
     request.session['customer_id'] = new_customer.id
 
     return redirect('/dashboard/products')
+
 
 def login(request):
     errors = Customer.objects.login_validator(request.POST)
@@ -72,7 +82,7 @@ def login(request):
             messages.error(request, errors[msg])
         return redirect('/')
 
-    all_customers = Customer.objects.filter(email = request.POST['email'])
+    all_customers = Customer.objects.filter(email=request.POST['email'])
     if len(all_customers) > 0:
         customer = all_customers[0]
         if bcrypt.checkpw(request.POST['password'].encode(), customer.password.encode()):
@@ -80,12 +90,15 @@ def login(request):
             return redirect('/dashboard/products')
     return redirect('/')
 
+
 def logout(request):
     request.session.clear()
     return redirect('/register_login')
 
 #####################################
 # Only Admins Can Access These Pages
+
+
 @login_required(login_url='/')
 # @allowed_users(allowed_roles=['admin'])
 def dashboard(request):
@@ -93,9 +106,10 @@ def dashboard(request):
     products = Product.objects.all()
     context = {
         'customers': customers,
-        'products' : products
+        'products': products
     }
     return render(request, 'dashboard/index.html', context)
+
 
 @login_required(login_url='/')
 def all_products(request):
@@ -105,6 +119,7 @@ def all_products(request):
     }
 
     return render(request, 'dashboard/products.html', context)
+
 
 @login_required(login_url='/')
 def showproduct(request, product_id):
@@ -117,6 +132,7 @@ def showproduct(request, product_id):
 
     return render(request, 'dashboard/singleproduct.html', context)
 
+
 @login_required(login_url='/')
 def editproduct(request):
     this_product = Product.objects.get(id=request.POST['product_id'])
@@ -124,7 +140,7 @@ def editproduct(request):
     this_product.desc = request.POST['desc']
     this_product.save()
 
-    Category.objects.create(name= request.POST['new_category'])
+    Category.objects.create(name=request.POST['new_category'])
     new_category = Category.objects.last()
     this_product.categories.add(new_category)
 
@@ -134,13 +150,15 @@ def editproduct(request):
 
     return redirect('dashboard/products')
 
+
 @login_required(login_url='/')
 def newproduct(request):
     categories = Category.objects.all()
     context = {
-        'categories' : categories
+        'categories': categories
     }
     return render(request, 'dashboard/newproduct.html', context)
+
 
 @login_required(login_url='/')
 def addnewproduct(request):
@@ -155,6 +173,7 @@ def addnewproduct(request):
     )
 
     return redirect('dashboard/products')
+
 
 @login_required(login_url='/')
 def deleteproduct(request):
